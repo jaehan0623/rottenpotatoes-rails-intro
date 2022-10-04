3db0578dea0bb5c6b7e52ad9ca02fa25d395146a
Jaehan Kim <jaehankim@berkeley.edu>
2022-10-04 01:30:58 +0000

    d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,17 +1,14 @@
 class Movie < ActiveRecord::Base
-  
-  def Movie.all_ratings
-    ['G','PG','PG-13','R']
-  end
+
   def Movie.with_ratings(ratings_to_show)
     display_rating1 = []
     ratings_to_show.each_with_index  do |val,index| 
       display_rating1[index] = val
      end
-    unless display_rating1.length == 0
-      where(rating:display_rating1)
+    if display_rating1.length == 0
+      Movie.where("")
     else
-      where("")
+      Movie.where(rating:display_rating1)
     end
   end
 end
