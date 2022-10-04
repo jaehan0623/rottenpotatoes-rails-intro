e31737a466dca1c620656b2289935b7a2d9a7369
Jaehan Kim <jaehankim@berkeley.edu>
2022-10-04 08:27:15 +0000

    will this work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,30 +6,38 @@ class MoviesController < ApplicationController
       # will render app/views/movies/show.<extension> by default
     end
   
+    def index
     
-  def index
-    
-    @all_ratings =  Movie.all_ratings
-    @param_ratings =  params[:ratings].nil? ? {} : params[:ratings]
-    @ratings_to_show = params[:ratings].nil? ?  @all_ratings : params[:ratings].keys ;
-
-
-    @sort = params[:sort].nil? ? "" : params[:sort]
-    
-    unless (params[:sort].present? && params[:ratings].present?)
-      h = {}.compare_by_identity
-      @all_ratings.each_with_index{|k,v| h[k] = v} 
-      redirect_to movies_path(sort: session[:sort] || "id" , ratings: session[:ratings] || h)
-      return
+      @all_ratings =  Movie.all_ratings
+      @param_ratings =  params[:ratings].nil? ? {} : params[:ratings]
+      @ratings_to_show = params[:ratings].nil? ?  @all_ratings : params[:ratings].keys ;
+  
+  
+      @sort = params[:sort].nil? ? "" : params[:sort]
       
+      unless (params[:sort].present? && params[:ratings].present?)
+        h = {}.compare_by_identity
+        @all_ratings.each_with_index{|k,v| h[k] = v} 
+        redirect_to movies_path(sort: session[:sort] || "id" , ratings: session[:ratings] || h)
+        return
+        
+      end
+      session[:ratings] =  params[:ratings]
+      session[:sort] = params[:sort]
+      
+      
+      @movies = Movie.with_ratings(@ratings_to_show).order(@sort)
     end
-    session[:ratings] =  params[:ratings]
-    session[:sort] = params[:sort]
-    
-    
-    @movies = Movie.with_ratings(@ratings_to_show).order(@sort)
-  end
 
+      @movies = @all_ratings
+      if session[:ratings].present?
+        @movies = Movie.with_ratings(@ratings_to_show)
+      end
+
+      if session[:sort]
+        @movies = @movies.order(@sort)
+      end
+    end
 
   
     def create
