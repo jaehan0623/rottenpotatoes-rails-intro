8e2c6fd512612c1c8614bcf3742c07f9e792adde
Jaehan Kim <jaehankim@berkeley.edu>
2022-10-04 10:13:05 +0000

    D

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,21 +12,17 @@ class MoviesController < ApplicationController
       @ratings_to_show = params[:ratings].nil? ?  @all_ratings : params[:ratings]
       @sort = params[:sort].nil? ? "" : params[:sort]
 
-      unless (params[:sort].present? && params[:ratings].present?)
-        h = {}.compare_by_identity
-        @all_ratings.each_with_index{|k,v| h[k] = v} 
-        redirect_to movies_path(sort: session[:sort] || "id" , ratings: session[:ratings] || h)
-        return
-      end
+
       # if params[:sort].nil? && params[:ratings].nil?
       #   @ratings_to_show = session[:ratings]
       #   @sort = session[:sort]
+      #   flash.keep
       #   redirect_to movies_path({sort: session[:sort], ratings: session[:ratings]})
       #   return 
       # end
 
-      session[:rating] = params[:ratings]
-      session[:sort] = params[:sort]
+      # session[:rating] = @ratings_to_show
+      # session[:sort] = @sort
 
       @movies = Movie.with_ratings(@ratings_to_show).order(@sort)
 
